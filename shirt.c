177868041926c8d353ed0c28e8c3fb93cbd3c0eb
Amit Mizrahi <amizra@outlook.com>
2013-06-14 20:32:51 -0400

    add t shirt code and instructions

---
+++ shirt.c
@@ -0,0 +1,28 @@
+																	
+																	
+																	
+																	
+																	
+																	
+																	
+																	
+																	
+																	
+																	#include     <stdio.h>
+																	#include     <stdlib.h>
+																	int main     ( void  ) {
+																	int a =      2  ; int  b;
+																	b = ++a;     int   awesome
+																	= b;;int     computers  = a;
+																	char *m;     m="IA Programming\
+																	 Club:\n     after school, 2:45\
+																	 in Room     408.\n";if(computers
+																    ==awesome    ) {printf("%s",  m );}
+																	exit(0);;    return  (  0   ); ; ;}
+
+
+															    
+															    
+																   // git clone https://github.com/iapc/tshirt
+																   // cd tshirt && make shirt
+																   // ./shirt
